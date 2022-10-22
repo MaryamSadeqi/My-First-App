78b32fc3f10877ae1a7e9b66f571ae5905031450
Maryam Sadeqi <themallaam@gmail.com>
2022-10-22 22:51:48 +0330

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Maryam Sadeqi on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
